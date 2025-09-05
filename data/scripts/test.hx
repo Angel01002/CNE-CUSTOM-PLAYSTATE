9b7952de7188027e088263b25e8c1fbbe446f5ba
Angel01002 <angelcontrujillo01112005@gmail.com>
2025-09-04 20:32:56 -0500

    Crear scripts/test.hx

---
+++ data/scripts/test.hx
@@ -0,0 +1,64 @@
+            import flixel.util.FlxTimeEventManager;
+            import flixel.util.FlxTimeEvent;
+            
+            var player:Character;
+            var point:FlxPoint;
+            var speed:Float;
+            
+            function postCreate():Void
+            {
+                player = this.strumLines.members[0].characters[0];
+                point = FlxPoint.get(player.x, player.y);
+                speed = 1;
+                
+                if (FlxTimeEvent.globalManager == null)
+                    FlxTimeEvent.globalManager = new FlxTimeEventManager();
+                
+                FlxTimeEvent.globalManager.clear();
+                FlxTimeEvent.globalManager.reset();
+                
+                FlxTimeEvent.tween(10000, 5000, {
+                    ease: FlxEase.quadInOut,
+                    onUpdate: function(_:FlxTimeEvent):Void {
+                        this.stage.stageSprites['bg'].angle = (0 + (360 * _.scale));
+                    },
+                    onComplete: function(_:FlxTimeEvent):Void {
+                        this.stage.stageSprites['bg'].angle = 0;
+                    }
+                });
+                
+                FlxTimeEvent.num(20000, 30000, 360, 0, {
+                    onUpdate: function(_:FlxTimeEvent):Void {
+                        this.stage.stageSprites['bg'].angle = _.value;
+                    }
+                });
+                
+                FlxTimeEvent.once(50000, function(_:FlxTimeEvent):Void {
+                    FlxG.camera.flash(FlxColor.RED, 2.0, null, true);
+                });
+                
+                if (this.curSongLower != "tutorial")
+                {
+                    FlxTimeEvent.globalManager.active = false;
+                }
+                else
+                {
+                    FlxTimeEvent.globalManager.start(-Conductor.crochet * 10);
+                }
+            }
+            
+            function update(elapsed:Float):Void
+            {
+                var sin:Float = Math.sin(Conductor.curBeatFloat) * speed;
+                player.x = point.x + sin;
+                player.y = point.y - sin;
+                FlxTimeEvent.globalManager.timeUpdate(Conductor.songPosition);
+            }
+            
+            function destroy():Void
+            {
+                point.put();
+                FlxTimeEvent.globalManager.clear();
+                FlxTimeEvent.globalManager.reset();
+                FlxTimeEvent.globalManager.pause();
+            }
